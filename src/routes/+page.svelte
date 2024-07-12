97fe461a56c966258b103c4b13fc6ab62784b4b5
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-07-12 07:26:56 +0200

    fix: home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 
 <div class='p-9'>
 <Heading tag="h1">Flowbite-Svelte Playground</Heading>
-<List>
-  <Li class="text-2xl">Fork this</Li>
+<List tag="ol">
+  <Li class="text-2xl">Fork this Stackblitz project</Li>
   <Li class="text-2xl">Update Flowbite-Svelte by running `pnpm i -D flowbite-svelte@latest`</Li>
-  <Li class="text-2xl">Run `pnpm updte` to update dependencies.</Li>
+  <Li class="text-2xl">Run `pnpm update` to update dependencies.</Li>
   <Li class="text-2xl">Add your code</Li>
 </List>
 
