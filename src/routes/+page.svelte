778303243f9579568b20d2a167927b844b95e09f
null <null>
2024-11-01 06:55:36 +0100

    chore: home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
   <Li class="text-2xl">Fork this Stackblitz project</Li>
   <Li class="text-2xl">Update Flowbite-Svelte by running `pnpm i -D flowbite-svelte@latest`</Li>
   <Li class="text-2xl">Run `pnpm update` to update dependencies.</Li>
-  <Li class="text-2xl">Add your code</Li>
+  <Li class="text-2xl">Add your code.</Li>
 </List>
 
 </div>
